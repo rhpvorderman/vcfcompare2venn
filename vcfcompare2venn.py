d61987b1acdc8cf0978fa36d3c461157d3ba01dd
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-04-05 12:22:00 +0200

    Make a three-way venn diagram

--- vcfcompare2venn.py
+++ vcfcompare2venn.py
@@ -17,10 +17,74 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
+import os
+import sys
 from typing import List, TextIO
 
+from matplotlib import pyplot as plt
 import matplotlib_venn
 
+
+def parse_label_filename(label: str):
+    filename, percentage = label.split()
+    return filename
+
+
 def subsets_from_vcf_compare_file(file: TextIO):
-    pass
+    lines = [line for line in file if line.startswith("VN\t")]
+    if len(lines) != 7:
+        raise NotImplementedError("Only venn3 diagrams are implemnted")
+    singles = []
+    duos = []
+    all = []
+    for line in lines:
+        spl = line.split("\t")
+        count = int(spl[1])
+        filenames = [parse_label_filename(x) for x in spl[2:]]
+        if len(filenames) == 1:
+            singles.append((filenames[0], count))
+        elif len(filenames) == 2:
+            duos.append((filenames, count))
+        elif len(filenames) == 3:
+            all.append((filenames, count))
+        else:
+            raise RuntimeError("Corrupt vcf-compare output.")
+    if len(singles) != 3 or len(duos) != 3 or len(all) != 1:
+        raise RuntimeError("Corrupt vcf-compare output")
+    duos_dict = {frozenset(filenames): count for filenames, count in duos}
+    _, all_count = all[0]
+    singles.sort()
+    a_name, a_count = singles[0]
+    b_name, b_count = singles[1]
+    c_name, c_count = singles[2]
+    a_and_b = frozenset((a_name, b_name))
+    a_and_c = frozenset((a_name, c_name))
+    b_and_c = frozenset((b_name, c_name))
+    subsets = (
+        a_count,
+        b_count,
+        duos_dict[a_and_b],
+        c_count,
+        duos_dict[a_and_c],
+        duos_dict[b_and_c],
+        all_count
+    )
+    labels = (
+        os.path.basename(a_name),
+        os.path.basename(b_name),
+        os.path.basename(c_name)
+    )
+    return subsets, labels
+
+
+def main():
+    with open(sys.argv[1]) as f:
+        subsets, lables = subsets_from_vcf_compare_file(f)
+    matplotlib_venn.venn3(subsets=subsets, set_labels=lables)
+    if len(sys.argv) == 2:
+        plt.show()
+    else:
+        plt.savefig(sys.argv[2])
+
+if __name__ == "__main__":
+    main()
